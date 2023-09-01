50133f4f006f0c5e5c7f20f728730011fa78d16d
Martin <martinbruun1995@gmail.com>
2023-09-01 09:51:49 +0200

    First commit

--- app.js
+++ app.js
@@ -1,1 +1,72 @@
-// skriv din JavaScript her...
+const contacts = [
+  {
+    id: 1,
+    firstName: "John",
+    lastName: "Doe",
+    email: "john.doe@example.com",
+    phone: "555-123-4567",
+  },
+  {
+    id: 2,
+    firstName: "Jane",
+    lastName: "Smith",
+    email: "jane.smith@example.com",
+    phone: "555-987-6543",
+  },
+  {
+    id: 3,
+    firstName: "Alice",
+    lastName: "Johnson",
+    email: "alice.johnson@example.com",
+    phone: "555-456-7890",
+  },
+  {
+    id: 4,
+    firstName: "Bob",
+    lastName: "Williams",
+    email: "bob.williams@example.com",
+    phone: "555-789-1234",
+  },
+  {
+    id: 5,
+    firstName: "Eva",
+    lastName: "Davis",
+    email: "eva.davis@example.com",
+    phone: "555-234-5678",
+  },
+  {
+    id: 6,
+    firstName: "Michael",
+    lastName: "Brown",
+    email: "michael.brown@example.com",
+    phone: "555-345-6789",
+  },
+  {
+    id: 7,
+    firstName: "Emily",
+    lastName: "Miller",
+    email: "emily.miller@example.com",
+    phone: "555-876-5432",
+  },
+  {
+    id: 8,
+    firstName: "Daniel",
+    lastName: "Martinez",
+    email: "daniel.martinez@example.com",
+    phone: "555-987-6543",
+  },
+  {
+    id: 9,
+    firstName: "Olivia",
+    lastName: "Clark",
+    email: "olivia.clark@example.com",
+    phone: "555-432-1098",
+  },
+  {
+    id: 10,
+    firstName: "William",
+    lastName: "Wilson",
+    email: "william.wilson@example.com",
+    phone: "555-210-9876",
+  },
+];
